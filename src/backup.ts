e1d3b59da8e6fdfb0c9fe464e5fef3d9ac059eba
Alex Bratsos <alexisbratsos@gmail.com>
2023-03-19 16:06:07 +0100

    yet another typo

--- src/backup.ts
+++ src/backup.ts
@@ -68,7 +68,7 @@ export const backup = async () => {
   const backupDir = env.BACKUP_DIR || '/tmp'
   const timestamp = date.replace(/[:.]+/g, '-')
   const filename = `backup-${timestamp}.tar.gz`
-  const filepath = `backup/${backupDir}/${filename}`
+  const filepath = `backups/${backupDir}/${filename}`
 
   console.log('dumping to file')
   await dumpToFile(filepath)
